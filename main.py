49fa71af1e00410faccf827e3470a6a071589b47
Elias <elijil000105@student.jenseneducation.se>
2026-06-08 13:19:43 +0200

    2.0 main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-print("1=Addition")
-print("2=Subtraktion")
+while True:
+    print("1=Addition")
+    print("2=Subtraktion")
 
-val = input("Välj 1 för addition eller 2 för subtraktion: ")
-tal1 = int(input("Skriv det första talet: "))
-tal2 = int(input("Skriv det andra talet: "))
+    val = input("Välj 1 för addition eller 2 för subtraktion: ")
+    tal1 = int(input("Skriv det första talet: "))
+    tal2 = int(input("Skriv det andra talet: "))
 
-if val == "1":
-    print("Resultat:", tal1 + tal2)
-else val == "2":
-    print("Resultat:", tal1 - tal2)
+    if val == "1":
+        print("Resultat:", tal1 + tal2)
+    elif val == "2":
+        print("Resultat:", tal1 - tal2)
+    else:
+        print("Ogiltigt val. Välj 1 eller 2.")
+
+    again = input("Vill du köra igen? (j/n): ").strip().lower()
+    if again != "j" and again != "ja":
+        print("Avslutar kalkylatorn.")
+        break
+    
     
